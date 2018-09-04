b2538cbad8f2ed8f654c5f2420c95ab8cddf9316
MaxAlexandrov <maximalexandrov87@gmail.com>
2018-09-04 20:10:40 +0600

    add code and mvc: dependence

--- MavenSpringProject/src/main/java/com/controller/UserController.java
+++ MavenSpringProject/src/main/java/com/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.controller;
 
+import com.model.User;
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
 /**
  * Created by max on 9/3/18.
  */
+@Controller
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
+    @RequestMapping("/list")
+    public @ResponseBody
+    List<User> getUsers(){
+        return userService.getUsres();
+    }
 }
